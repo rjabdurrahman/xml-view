18d424d2444527649e4723f0a9031edc4ed9b50b
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-02-23 00:11:36 +0600

    Updated: date

--- helpers/today.js
+++ helpers/today.js
@@ -1,4 +1,4 @@
 module.exports = function (info) {
     let data = new Date();
-    return `${data.getFullYear()}-${("0" + (data.getMonth() + 1)).slice(-2)}-${("0" + data.getDate()).slice(-2)}`;
+    return `${data.getFullYear()}-${("0" + (data.getMonth() + 1)).slice(-2)}-${("0" + (data.getDate() + 1)).slice(-2)}`;
 }
